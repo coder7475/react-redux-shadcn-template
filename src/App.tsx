2af1fbe72a211ff07a7041b85de1389e99b70ba7
coder7475 <robiulhossain7475@gmail.com>
2025-06-30 12:36:51 +0600

    feat: setup shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div>
       <h1>Hello Frontend Template!</h1>
+      <Button>Click Me</Button>
     </div>
   );
 }
