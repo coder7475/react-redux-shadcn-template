6266d92cac306c11f735da32e925532b4ae46834
coder7475 <robiulhossain7475@gmail.com>
2025-08-08 18:20:14 +0600

    feat: setup axios, redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,19 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "@/redux/slices/counterSlice";
-import { pokemonApi } from "@/services/pokemon";
+import { baseApi } from "./baseApi";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-    [pokemonApi.reducerPath]: pokemonApi.reducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
-  // Adding the api middleware enables caching, invalidation, polling,
-  // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }).concat(pokemonApi.middleware),
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
+
+setupListeners(store.dispatch);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
